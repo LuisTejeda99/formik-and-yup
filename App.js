21362818c31992b6ddc101ffda16d27f14111a2c
Luis Manuel Tejeda Cano <luistejeda@MacBook-Pro-de-Luis.local>
2020-11-02 19:11:38 -0600

    re-using components and completed project

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import {Formik, useFormikContext, useField} from "formik";
+import * as Yup from "yup";
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+const MyInput = ({ fieldName, ...props }) => {
+  const [field, meta] = useField(fieldName);
+
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        onChangeText={field.onChange(fieldName)}
+        value={field.value}
+        onBlur={field.onBlur(fieldName)}
+        {...props}
+        />
+        {
+          meta.error && meta.touched && 
+          (<Text style={{color: "red"}}> {meta.error} </Text>)
+        } 
+    </>
+    );
+  }
+  
+  const EmailForm = () => {
+  const {submitForm} = useFormikContext();
+  return (
+    <>
+      <Text> Correo Electronico </Text>
+      <MyInput fieldName="email"/>
+      <MyInput fieldName="name"/>
+      <Button onPress={submitForm} title="Envíar"/>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Formik
+        onSubmit={x => console.log(x)}
+        initialValues={{email:"",name:""}}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Correo invalido")
+            .required("Requerido"),
+          name: Yup.string()
+            .min(10)
+            .required("Requerido")
+        })}>
+        <EmailForm />
+      </Formik>
+    </View> 
   );
 }
 
@@ -17,5 +61,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 30,
+    alignSelf:"stretch",
+    backgroundColor: "#eee",
   },
 });
